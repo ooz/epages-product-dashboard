b7a6721041671c00196d4d6b24ccbe35cef9bcd8
Oliver Zscheyge <o.zscheyge@epages.com>
2017-10-18 11:24:33 +0200

    Initial commit

---
+++ src/main/java/com/epages/productdashboard/ProductDashboardApplication.java
@@ -0,0 +1,12 @@
+package com.epages.productdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductDashboardApplication.class, args);
+	}
+}
